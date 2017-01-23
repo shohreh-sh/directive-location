f6f9f6a3d44c729543e0f6018cc21e288823fa52
shohreh <shohre.shadvalian.1@gmail.com>
2017-01-24 00:02:47 +0330

    project skeleton

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name locationDirectiveApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the locationDirectiveApp
+ */
+angular.module('locationDirectiveApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name locationDirectiveApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the locationDirectiveApp
+ */
+angular.module('locationDirectiveApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
